01f8fc469e0c461005695075284289473ea55f44
Owen <turner@roe.ac.uk>
2015-02-10 14:15:37 +0000

    comparing master and testing branches

--- Python_code/Instances/classTest.py
+++ Python_code/Instances/classTest.py
@@ -42,12 +42,14 @@ newFile = '/Users/owenturner/Documents/PhD/KMOS/KMOS_DATA/NGC55/14-9-2014/KMOS_S
 #pipe_methods.plotMedian('KMOS_SPEC_OBS258_0009_m_8_raw.fits', 'KMOS_SPEC_OBS258_0009_m_8.fits', \
 # 'segmentsSubtracted_9_m_8_128.fits', 'topFour_9_m_8.fits', 400, 600, 800, 1270)
 
+rho = pipe_methods.crossCorr(ext=1, objFile=objFile, skyFile=skyFile, y1=100, y2=1800, x1=100, x2=1800)
+print rho
 #pipe_methods.shiftImage(ext=1, infile=objFile, skyfile=skyFile, interp_type = 'linear',stepsize=0.03, xmin=-0.2, xmax=0.2, ymin=-0.2, ymax=0.2)
 #pipe_methods.rotateImage(ext=1, infile=objFile, skyfile=skyFile, interp_type = 'linear', stepsize=0.001, minAngle=-0.01, maxAngle=0.01)
 #array = pipe_methods.imSplit(ext=1, infile=objFile, vertSegments=4, horSegments=4)
-pipe_methods.shiftImageSegments(ext=1, infile=objFile, skyfile=skyFile,\
-  	 vertSegments=2, horSegments=2, interp_type='linear', \
-  	 stepsize=0.01, xmin=-0.1, xmax=0.1, ymin=-0.1, ymax=0.1)
+#pipe_methods.shiftImageSegments(ext=1, infile=objFile, skyfile=skyFile,\
+#  	 vertSegments=2, horSegments=2, interp_type='linear', \
+#  	 stepsize=0.01, xmin=-0.1, xmax=0.1, ymin=-0.1, ymax=0.1)
 
 
 
